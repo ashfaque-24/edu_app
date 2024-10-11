17f9808293f54a4fdfbae7a738e0fbae13f2e04f
ashfaque-24 <ashfaque2407@gmail.com>
2024-10-11 11:32:24 +0000

    Your commit message describing the changes

---
+++ edu_app/education_app/doctype/transport_module/test_transport_module.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Ashfaque Mustakim Ansari and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTransportModule(FrappeTestCase):
+	pass
---
+++ edu_app/education_app/doctype/transport_module/transport_module.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ashfaque Mustakim Ansari and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Transport Module", {
+// 	refresh(frm) {
+
+// 	},
+// });
